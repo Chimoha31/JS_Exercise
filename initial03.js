02b3603a2fade7c02a60be71b75faf505443be56
Chimoha31 <mokochii1108@gmail.com>
2022-02-04 23:18:11 -0800

    Done 4-5

--- initial03.js
+++ initial03.js
@@ -43,4 +43,19 @@ console.log(ascending([1, 2, 4, 3, 5]));
 console.log(ascending([1, 1, 2, 3, 3]));
 
 // 4, Write a JS program to get the largest even number from an array of integer.
+const largestrEvenNum = (arr) => {
+  return Math.max(...arr.filter(num => num % 2 === 0));
+}
 
+console.log(largestrEvenNum([11, 22, 33, 46, 88, 100]));
+console.log(largestrEvenNum([8, 1, 624, 66, 51, 44, 777]));
+
+// 5, Write a JS program to replace the first digit in a string (should contains at least digit) with $ character.
+const replaceNumWith$ = (arr) => {
+  return arr.replace(/[0-9]/, '$');
+}
+
+
+console.log(replaceNumWith$('abcd5ef080'));
+console.log(replaceNumWith$('Atin3der787'));
+console.log(replaceNumWith$('Tedd1y555'));
