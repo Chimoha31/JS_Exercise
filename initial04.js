e48f33a74372edcfcb120392cb495d5101c6ffea
Chimoha31 <mokochii1108@gmail.com>
2022-02-06 12:35:33 -0800

    Done 2-3

--- initial04.js
+++ initial04.js
@@ -14,3 +14,31 @@ console.log(isLeapYear(2023));
 console.log(isLeapYear(2024));
 
 //2, Write a JS program to compare two objects to determine if the first one contains the same properties as the second one (which may also have additional properties)
+
+const objA = {a:'banana', b:'apple', c:'cherry'};
+const objB = {a:'melon', e:'lemmon', d:'tomato'};
+const objC = {a:'kiwi', b:'pinapple', c:'peach'};
+
+const matchKey = (obj1, obj2) => {
+  return Object.keys(obj1).every(key => obj2[key])
+}
+
+console.log(matchKey(objA, objB));
+console.log(matchKey(objA, objC));
+console.log(matchKey(objB, objC));
+
+//3, Write a JS program to convert a comma-separated values(CSV) string to a 2D array. A new line indicates a new row in the array.
+// Example: 
+// abc, def, ghi
+// jkl, mno, pqr
+// stu, vwx,byza
+const convertCSV = (str) => {
+  return str.split('\n').map(row => row.split(','))
+};
+
+const strForParam = `abc, def, ghi 
+jkl, mno, pqr 
+stu, vwx,byza`;
+
+console.log(convertCSV(strForParam));
+
